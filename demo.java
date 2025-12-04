a8ec1637f4e0d34571588f897aa0608a85da1d84
Pradnyakiran27 <naikpradnya64@gmail.com>
2025-12-04 10:09:07 +0530

    Remove Jenkins job print statement

--- demo.java
+++ demo.java
@@ -3,7 +3,6 @@ public class demo
          public static void main(String[] args) {
              System.out.println("Hello, World!");
           System.out.println("Hello, pradnya");
-          System.out.println("performing jenkinns job");
           System.out.println("TASK-4");
          }
      }
